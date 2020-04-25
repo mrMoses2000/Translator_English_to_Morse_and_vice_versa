1c8c6c97b860421a554556464d4f21f3daae2e31
Moses <54993333+mrMoses2000@users.noreply.github.com>
2020-04-25 17:19:50 +0700

    Add files via upload

---
+++ Statistics.java
@@ -0,0 +1,39 @@
+package TmpPack;
+
+class Statistics{
+    int counter;
+    char ch;
+    Statistics (char ch)
+    {
+        counter = 1;
+        this.ch = ch;
+    }
+    @Override
+    public int hashCode(){
+        return ch;
+    }
+    @Override
+    public boolean equals(Object o)
+    {
+        Statistics other = (Statistics) o;
+        if (this == o){
+            other.counter++;
+            this.counter = other.counter;
+            return true;
+        }
+        if (o == null)
+            return false;
+        if (getClass() != o.getClass())
+            return false;
+        if (this.ch != other.ch)
+            return false;
+        other.counter++;
+        this.counter = other.counter;
+        return true;
+    }
+    public String print()
+    {
+        System.out.println(this.ch);
+        return "<" + Character.toString(this.ch) + ">: " + this.counter;
+    }
+}
